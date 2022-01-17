3862613fbb832e5670ae84d3c6586cb202fd7ffe
Mercedes <mercedeswagner84@gmail.com>
2022-01-17 16:59:30 +0100

    title/js merged all tasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,193 @@
+console.log("=== Aufgabe 1 intro_Lev1_7 ===")
+
+
+// console.log("test");
+
+// console.log("=== Aufgabe 1 ===")
+
+// let helloworld = "Hello World";
+// console.log(helloworld);
+
+// const pi = 3.14;
+// console.log(pi);
+
+// const burjdubai = "828m";
+// console.log(burjdubai);
+
+// let fullName = "Jan Schmidt";
+// console.log(fullName);
+
+// const eifeltower = "324m";
+// console.log(eifeltower);
+
+// const camelcase = "camelCase";
+// console.log(camelcase);
+
+// let kursstatus = true;
+// console.log(kursstatus);
+
+
+
+console.log("=== Aufgabe 2 console.log_Lev1_1 ===")
+
+
+// a = 12;
+// b = a * 3;
+// console.log(b);
+
+// document.write(b);
+
+// console.log(document.URL);
+
+// console.log("Hello World");
+
+// console.log("Anton");
+
+// let Name = "Anton"
+// console.log(name);
+
+// let age = 28
+// console.log(age);
+
+// let job = "Trainer"
+// console.log(job);
+
+// var married = true
+// console.log(married);
+
+// console.log(Name + " ist " + age + " Jahre alt und ist verheiratet: " + married)
+
+
+console.log("=== Aufgabe 3 variables_Lev1_5 ===")
+
+
+// let carName = "BMW"
+// console.log(carName);
+
+// let x = 150
+// console.log(x);
+
+// let y = 50;
+// console.log(y);
+
+// let z = x + y;
+// console.log(z);
+
+// let firstName = "John"; lastName = "Doe"; age = 35;
+// console.log(firstName + " " + lastName + ", " + age);
+
+
+
+console.log("=== Aufgabe 4 let_const_var_Lev1_6 ===")
+
+
+// var z = 34;
+// z = 67;
+// console.log(z);
+
+// let x = 34;
+// x = 67;
+// console.log(x);
+
+// const y = 34;
+// y = 67;
+// console.log(y);
+
+
+
+console.log("=== Aufgabe 5 arithmethic-operators_Lev1_12 ===")
+
+
+// // Multiplikation geht vor und so wird 5 * 10 zuerst durchgeführt.
+// let score1 = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score1);
+
+// // Du kannst Klammern() hinzufügen, um die Reihenfolge der Prioritäten zu ändern.
+// let score2 = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score2);
+
+// // Du kannst Werte auf Variablen addieren.
+// let score3 = 0;
+// score4 = score3 + 10;
+// console.log('Ergebnis: ' + score4);
+
+// // Hiervon gibt es eine verkürzte Version.
+// score3 += 10;
+// console.log('Ergebnis: ' + score3);
+
+// // Inkrementierung und Dekrementierungen können wie folgt verwendet werden:
+// let zahl = 1;
+// zahl = zahl + 1;
+// zahl += 1;
+
+// // Verwende Inkrement - Operatoren
+// zahl++;
+// console.log('increment: ' + zahl);
+
+// // Verwende Dekrement - Operatoren
+// zahl--;
+// console.log('dekrement: ' + zahl);
+
+
+
+console.log("=== Aufgabe 6 arithmethic-repetition_Lev1_13 ===")
+
+
+// // Verwende Grundrechenarten
+// // Addition
+// let addition_operator = 1 + 1;
+// console.log("addition: " + addition_operator);
+
+// // Subtraktion
+// let subtraktion_operator = 2 - 1;
+// console.log("subtraktion: " + subtraktion_operator);
+
+// // Multiplikation
+// let multiplication_operator = 2 * 2;
+// console.log("multiplication: " + multiplication_operator);
+
+// // Division
+// let division_operator = 4 / 2;
+// console.log("division: " + division_operator);
+
+// // Modul: zeigt an, was der Rest sein würde.
+// let modulus_operator = 14 % 5;
+// console.log("modulus: " + modulus_operator);
+
+
+console.log("=== Aufgabe 7 document-write_Lev1_1 ===")
+
+// document.write("<h1> Hello World!</h1>");
+
+// document.write("<p> Have a nice day! </p>");
+
+// let myText = "Hello again!";
+// document.write(myText);
+
+// let Name = "Mercedes";
+// document.write("<p>Name: " + Name);
+
+// let Nachname = "Wagner";
+// document.write("<p>Nachname: " + Nachname);
+
+
+console.log("=== Aufgabe 8 String-Methods_Lev1_11 ===")
+
+// let x = "Hello ";
+// let y = "World<br>";
+
+// z = x + y
+// document.write(z);
+
+// n = x + " " + y;
+// document.write(n);
+
+// x += "World"
+// document.write(x);
+
+
+console.log("=== Aufgabe 9 window-object-method_Lev1_4 ===")
+
 // window.alert("Hallo Welt");
 
 // window.prompt("Bitte geben Sie Ihren Namen ein.");
@@ -23,36 +213,4 @@
 
 // alert(location.hostname);
 
-// let person = prompt("Please enter your name", "Harry Potter");
-
-// if (person != null) {
-//     document.getElementById("demo").innerHTML =
-//         "Hello " + person + "! How are you today?";
-// }
-
-let text1;
-let favDrink = prompt("What's your favorite cocktail drink?");
-switch (favDrink) {
-    case "Coca-Cola":
-        text = "Excellent choice! Coca-Cola is good for your soul.";
-        break;
-    case "Pepsi":
-        text = "Pepsi is my favorite too!";
-        break;
-    case "Sprite":
-        text = "Really? Are you sure the Sprite is your favorite?";
-        break;
-    default:
-        text = "I have never heard of that one!";
-}
-
-confirm("Press a button!");
-
-confirm("Press a button!\nEither OK or Cancel.");
-
-let text2;
-if (confirm("Press a button!") == true) {
-    text = "You pressed OK!";
-} else {
-    text = "You canceled!";
-}
+let person = prompt("Please enter your name", "Harry Potter");
